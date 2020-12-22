73fae4a043f2421538a589a7c50f35f31e16dc1d
25093yukinoji <ten.yuki.0601@gmail.com>
2020-12-22 15:13:36 +0000

    [Add]6-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd075bd5b3b8086e5867d8138889c7dd83634e4779a5049b5161fea83b78868aae1e9ee56914ac1af3e7011c0c77b5f0cb0a2c7c5867008cbd4076b4c6a706d20'
